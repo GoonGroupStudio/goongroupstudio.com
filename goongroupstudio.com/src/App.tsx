253c66fd767df24c0da69a17059b78cc33953b6a
Mr Farmer 2 <74437289+mrfarmer2@users.noreply.github.com>
2023-02-05 09:10:23 -0600

    v1.0

--- goongroupstudio.com/src/App.tsx
+++ goongroupstudio.com/src/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Hello World!
-        </p>
-
-      </header>
-    </div>
+      <>
+      <Nav/>
+        <h1>Hello World</h1>
+      </>
   );
-  
 }
 
 export default App;
