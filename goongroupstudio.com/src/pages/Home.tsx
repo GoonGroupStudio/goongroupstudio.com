5c22a477a4af9cc4e1557de473086eb5a80802e0
Mr Farmer <56373526+mrfarmer09@users.noreply.github.com>
2023-02-08 20:02:30 -0600

    v1.0 (Doesn't work yet)

---
+++ goongroupstudio.com/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '/Users/Mr Farmer/Documents/My Website/React/goongroupstudio.com/goongroupstudio.com/src/components/Nav/Nav';
+
+function Home() {
+  return (
+    <>
+      <Nav />
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default Home;
